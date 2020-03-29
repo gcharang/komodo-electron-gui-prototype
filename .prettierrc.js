f1957701e5e44fb7bcc991a1aa07dfdd9254ce8c
gcharang <mrgcharang@gmail.com>
2020-03-30 00:13:11 +0530

    adds prettier config and vscode settings

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  arrowParens: "always",
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
