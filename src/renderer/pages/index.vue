19447569967e824194cf1f2fffcd23345d5a346e
gcharang <mrgcharang@gmail.com>
2020-04-02 20:44:01 +0530

    binary verifier(shasum) done. really this time. only shasum no sig tho

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,107 +1,101 @@
 <template>
   <v-row justify="center">
     <v-col cols="auto">
-      <v-card width="1600" height="800" raised>
-        <v-card-title>Settings</v-card-title>
-        <v-col cols="auto">
-          <v-card width="450" height="250" raised>
-            <v-toolbar>
-              <v-toolbar-title>Select the Komodo Daemon</v-toolbar-title
-              ><v-spacer />
-              <v-tooltip top>
-                <template v-slot:activator="{ on }">
-                  <v-switch
-                    v-model="komododSelected"
-                    :disabled="!fileIsValid"
-                    inset
-                    hide-details
-                    v-on="on"
-                    ><template v-slot:prepend>
-                      <v-icon v-if="komododSelected" color="success"
-                        >mdi-lock-check
-                      </v-icon>
-                      <v-icon v-else color="error"
-                        >mdi-lock-open-variant
-                      </v-icon>
-                    </template>
-                  </v-switch>
+      <v-card width="450" height="250" raised>
+        <v-toolbar>
+          <v-toolbar-title>Select the Komodo Daemon</v-toolbar-title
+          ><v-spacer />
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-switch
+                v-model="komododSelected"
+                :disabled="!fileIsValid"
+                inset
+                hide-details
+                v-on="on"
+                ><template v-slot:prepend>
+                  <v-icon v-if="komododSelected" color="success"
+                    >mdi-lock-check
+                  </v-icon>
+                  <v-icon v-else color="error">mdi-lock-open-variant </v-icon>
                 </template>
-                <span>{{
-                  komododSelected
-                    ? "Click the switch to select a DIFFERENT File"
-                    : "Click the switch to CONFIRM your selection"
-                }}</span>
-              </v-tooltip>
-            </v-toolbar>
-            <br />
-            <v-file-input
-              v-model="chosenFile"
-              label="Click here to select the Komodo Daemon"
-              outlined
-              :disabled="komododSelected"
-              :error-messages="error"
-              @click="clearError"
-              @click:clear="clearError"
-            />
-            <blockquote v-if="fileIsValid">
-              The path of the selected Komodo Daemon is
-              <b>{{ chosenFile.path }}</b>
-            </blockquote>
-          </v-card>
-        </v-col>
-        <v-col cols="auto">
-          <v-card width="450" height="350" raised>
-            <v-toolbar>
-              <v-toolbar-title>Verify Authenticity</v-toolbar-title><v-spacer />
-              <v-tooltip top>
-                <template v-slot:activator="{ on }">
-                  <v-checkbox
-                    v-model="verifyAuthenticityEnabled"
-                    hide-details
-                    :disabled="!komododSelected"
-                    v-on="on"
-                  >
-                  </v-checkbox>
-                </template>
-                <span>{{
-                  verifyAuthenticityEnabled
-                    ? "Click to  Disable"
-                    : "Click to Enable"
-                }}</span>
-              </v-tooltip>
-            </v-toolbar>
-            <br />
-            <v-text-field
-              v-model="expectedShaSum"
-              :disabled="verifyAuthTextFieldDisabled"
-              label="Expected SHASUM"
-            ></v-text-field>
-            <v-text-field
-              v-model="signerPubkey"
-              :disabled="true"
-              label="Signer's Pubkey"
-            ></v-text-field>
-            <v-text-field
-              v-model="expectedSignature"
-              :disabled="true"
-              label="Expected Signature"
-            ></v-text-field>
-            <v-card-actions>
-              <blockquote v-if="shaSumMatches">
-                The SHA256 checksum matches
-              </blockquote>
-              <v-spacer />
-              <v-btn
-                :disabled="verifyButtonDisabled"
-                color="indigo"
-                @click="verifyShaSum"
+              </v-switch>
+            </template>
+            <span>{{
+              komododSelected
+                ? "Click the switch to select a DIFFERENT File"
+                : "Click the switch to CONFIRM your selection"
+            }}</span>
+          </v-tooltip>
+        </v-toolbar>
+        <br />
+        <v-file-input
+          v-model="chosenFile"
+          label="Click here to select the Komodo Daemon"
+          outlined
+          :disabled="komododSelected"
+          :error-messages="error"
+          @click="clearError"
+          @click:clear="clearError"
+        />
+        <blockquote v-if="fileIsValid">
+          The path of the selected Komodo Daemon is
+          <b>{{ chosenFile.path }}</b>
+        </blockquote>
+      </v-card>
+    </v-col>
+    <v-col cols="auto">
+      <v-card width="450" height="350" raised>
+        <v-toolbar>
+          <v-toolbar-title>Verify Authenticity</v-toolbar-title><v-spacer />
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-checkbox
+                v-model="verifyAuthenticityEnabled"
+                hide-details
+                :disabled="!komododSelected"
+                v-on="on"
               >
-                <v-icon>mdi-shield-check</v-icon>
-                <span>&nbsp;Verify</span>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+              </v-checkbox>
+            </template>
+            <span>{{
+              verifyAuthenticityEnabled
+                ? "Click to  Disable"
+                : "Click to Enable"
+            }}</span>
+          </v-tooltip>
+        </v-toolbar>
+        <br />
+        <v-text-field
+          v-model="expectedShaSum"
+          :disabled="expectedShaSumIsDisabled"
+          label="Expected SHASUM"
+        ></v-text-field>
+        <v-text-field
+          v-model="signerPubkey"
+          :disabled="true"
+          label="Signer's Pubkey"
+        ></v-text-field>
+        <v-text-field
+          v-model="expectedSignature"
+          :disabled="true"
+          label="Expected Signature"
+        ></v-text-field>
+        <v-card-actions>
+          <blockquote v-if="displayShaSumResult">
+            The SHA256 checksum
+            {{ shaSumMatches ? "matches" : "doesn't match" }}
+          </blockquote>
+          <v-spacer />
+          <v-btn
+            :disabled="verifyButtonDisabled"
+            color="indigo"
+            @click="verifyShaSum"
+          >
+            <v-icon>mdi-shield-check</v-icon>
+            <span>&nbsp;Verify</span>
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
 
@@ -138,6 +132,8 @@ export default {
       shasumIsValid: false,
       signatureIsValid: false,
       shaSumMatches: null,
+      calcFileHashIsRunning: false,
+      displayShaSumResult: false,
     };
   },
   computed: {
@@ -176,6 +172,9 @@ export default {
     verifyButtonDisabled() {
       return this.verifyAuthTextFieldDisabled;
     },
+    expectedShaSumIsDisabled() {
+      return this.verifyAuthTextFieldDisabled || this.calcFileHashIsRunning;
+    },
   },
   watch: {
     chosenFile(val) {
@@ -207,6 +206,9 @@ export default {
         this.errors = [];
       }
     },
+    expectedShaSum(val) {
+      this.displayShaSumResult = false;
+    },
   },
   methods: {
     clearError() {
@@ -214,8 +216,11 @@ export default {
     },
     async verifyShaSum() {
       try {
+        this.calcFileHashIsRunning = true;
         const hash = await this.calcFileHash(this.filePath);
+        this.calcFileHashIsRunning = false;
         this.shaSumMatches = hash === this.expectedShaSum;
+        this.displayShaSumResult = true;
         console.log(hash);
         console.log(
           `this.shaSumMatches = hash === this.expectedShaSum;: ${
